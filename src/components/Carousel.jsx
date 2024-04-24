f773ae312743f7772fa5431a97dfc10329fe87ab
tdunca <dunca.theresa@gmail.com>
2024-04-24 14:17:24 +0200

    Updated projects page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import projectone from "../images/projectone.jpg"
-import projecttwo from "../images/projecttwo.jpg"
-import projectthree from "../images/projectthree.jpg"
+import projectone from "../images/memory.jpg"
+import projecttwo from "../images/backend.jpg"
+import projectthree from "../images/ux-design.jpg"
 
 const CarouselComponent = () => {
   return (
     <Carousel>
       <div class>
-         <img src={projectone} alt='projectone' />
-		 <p className='item'><a className="carousel-link" href="https://github.com/">P R O J E C T O N E</a></p>
+         <img src={projectone} alt='memory-project' />
+		 <p className='item'><a className="carousel-link" href="https://tdunca.github.io/MemoryGame-Assignment/">M E M O R Y - A S G M E N T</a></p>
        </div>
        <div>
-         <img src={projecttwo} alt='projecttwo' />
-		 <p className='item'><a className="carousel-link" href="https://github.com/">P R O J E C T T W O</a></p>
+         <img src={projecttwo} alt='backend-project' />
+		 <p className='item'><a className="carousel-link" href="https://github.com/tdunca/Backend-FE-23">B A C K E N D - A S G M E N T</a></p>
        </div>
       <div>
 	  <img src={projectthree} alt="projecthree"/>
-	  <p className='item'><a className="carousel-link" href="https://github.com/">P R O J E C T T H R E E</a></p>
+	  <p className='item'><a className="carousel-link" href="https://www.figma.com/proto/KJdk0n042hfUpsMnwCWIED/UX-Design-Assignment?type=design&node-id=6-31&t=mN8fWmKeb0aJTuUP-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=6%3A31&mode=design">U X D E S I G N - A S G M E N T </a></p>
      </div>
    </Carousel>
   );
